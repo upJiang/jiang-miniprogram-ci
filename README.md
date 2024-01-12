6131a9773717f1f05f308b9d022316cf04b956e8
江俊锋 <jiangjf04@vanke.com>
2024-01-12 17:35:03 +0800

    feat(init): 完成

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # jiang-miniprogram-ci
-自己做的 github，miniprogram-ci，由于社区中的小程序集成ci都不太满足自己的需求
+
+基于 `miniprogram-ci` 的一款微信小程序发布版本的自动集成
+
+使用方式：
+
+- 下载微信小程序的小程序代码上传密钥，并且关闭 `IP` 白名单
+
+![image.png](https://p9-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/95619488f4c64860b2ac2720b7ba6045~tplv-k3u1fbpfcp-jj-mark:0:0:0:0:q75.image#?w=1304&h=358&s=22093&e=png&b=ffffff)
+
+- 在 `GitHub` 项目中的 `setting` 添加一个变量名 PRIVATE_KEY，写入上一步获取的小程序代码上传密钥
+
+![image.png](https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/af41b0ecf3f14e3f823766126481a342~tplv-k3u1fbpfcp-jj-mark:0:0:0:0:q75.image#?w=1253&h=822&s=99090&e=png&b=ffffff)
+
+- 需要添加的参数：
+  - **projectPath**（必填）：项目路径，比如 `dist`、`uniapp` 项目一般是 `dist/build/mp-weixin`
+  - **privatekey**（必填）：前面设置的 `PRIVATE_KEY`，如：${{ secrets.PRIVATE_KEY }}
+  - **robot**：机器人编号，默认是 1
+  - **desc**：本次提交的修改描述
+  - **version**：本次提交的版本号
+
+`.github/workflows/upload.yaml` 写入以下代码
+
+```
+name: 上传小程序体验版
+on:
+  push:
+    branches:
+      - main
+jobs:
+  upload:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v3
+      - name: Install
+        run: yarn
+      - name: Build
+        run: yarn build
+      - name: Upload
+        uses: jiang/jiang-miniprogram-ci@main
+        with:
+          mode: upload
+          project-path: dist/build/mp-weixin
+          private-key: ${{ secrets.PRIVATE_KEY }}
+```
---
+++ index.js
@@ -0,0 +1,65 @@
+const core = require("@actions/core");
+const fs = require("fs");
+const path = require("path");
+import * as ci from "miniprogram-ci";
+
+async function run() {
+  try {
+    const projectPath = core.getInput("projectPath", { required: true });
+    const privatekey = core.getInput("privatekey", { required: true });
+
+    // 构建完整的 project.config.json 路径
+    const projectConfigPath = path.join(projectPath, "project.config.json");
+
+    // 读取和解析 project.config.json 文件
+    const projectConfig = JSON.parse(
+      fs.readFileSync(projectConfigPath, "utf8")
+    );
+
+    // 从 project.config.json 获取需要的配置，这里拿到 appid
+    const appid = projectConfig.appid;
+
+    // 版本信息
+    const version = core.getInput("version") || "1.0.0";
+    // 本次提交的描述
+    const description = core.getInput("description") || "更新部分功能";
+
+    // 创建 Project 实例
+    const project = new ci.Project({
+      appid,
+      type: "miniProgram",
+      projectPath,
+      privateKeyPath: privatekey,
+      ignores: ["node_modules/**/*"],
+    });
+
+    // 其他配置
+    const context = {
+      compileOptions: {}, // 编译配置
+      robot: core.getInput("robot") || 1, // 机器人编号
+      threads: 1, // 线程数
+      allowIgnoreUnusedFiles: false, // 允许忽略未使用的文件
+    };
+
+    // 上传进度的回调函数
+    const onProgressUpdate = (info) => {
+      console.log("上传进度：", info);
+    };
+
+    // 执行上传
+    await ci.upload({
+      project,
+      version,
+      desc: description,
+      setting: context.compileOptions,
+      robot: context.robot,
+      threads: context.threads,
+      allowIgnoreUnusedFiles: context.allowIgnoreUnusedFiles,
+      onProgressUpdate,
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
